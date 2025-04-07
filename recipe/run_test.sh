51d08af86f2dea65bab3a902af25b043d43541e7
Min RK <benjaminrk@gmail.com>
2025-04-07 09:02:19 +0200

    don't need outdated mpiexec wrapper

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -23,9 +23,6 @@ else
     make test10
     make test14f
 
-    # FIXME: Workaround mpiexec setting O_NONBLOCK in std{in|out|err}
-    # See https://github.com/conda-forge/conda-smithy/pull/337
-    # See https://github.com/pmodels/mpich/pull/2755
-    make runtest10  MPIEXEC="${RECIPE_DIR}/mpiexec.sh"
-    make runtest14f MPIEXEC="${RECIPE_DIR}/mpiexec.sh"
+    make runtest10
+    make runtest14f
 fi
